2072284dd24e7f036340b14a8a5d1a6917074944
vovanolo <vovchik.student221@gmail.com>
2021-02-09 12:26:04 +0200

    lesson5

--- src/index.js
+++ src/index.js
@@ -1,19 +1,78 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 
-const link = React.createElement(
-  "a",
-  { href: "http://www.google.com" },
-  "Посилання на гугл"
-);
+// const title = React.createElement("h1", null, "my title");
 
-const header = React.createElement("h1", {}, "Заголовок сайту");
+// const link = React.createElement(
+//   "a",
+//   {
+//     href: "#",
+//     target: "_blank",
+//   },
+//   "link"
+// );
 
-const headerTwo = React.createElement("h2", {}, "заголовок номер два");
+// const block = React.createElement("div", null, title, link);
 
-const block = React.createElement("div", null, header, headerTwo, link);
+// const block = React.createElement("div", {
+//   children: [title, link],
+// });
 
-ReactDOM.render(block, document.getElementById("root"));
+// const link = React.createElement(
+//   "a",
+//   {
+//     href: "http://www.google.com",
+//     target: "_blank",
+//   },
+//   "link"
+// );
 
-// домашнє завдання
-// створити елемент nav всередині якого буде список ul з чотирма елементами li
+// const linkWithJSX = (
+//   <a href="http://www.google.com" target="_blank">
+//     link
+//   </a>
+// );
+
+// const block = (
+//   <div>
+//     <h1>my title</h1>
+//     <a href="http://www.google.com" target="_blank">
+//       link
+//     </a>
+//   </div>
+// );
+
+// const text = "new header";
+
+// const description = "sdafasfasfasfsfasfas";
+
+// const google = "http://www.google.com";
+
+// const element = (
+//   <>
+//     <a href={google}>link to google</a>
+//     <h2>{text}</h2>
+//     <p>{description}</p>
+//   </>
+// );
+
+// const Product = ({ price, name, imgUrl }) => (
+//   <div>
+//     <img src={imgUrl} />
+//     <h2>{name}</h2>
+//     <p>price: {price}$</p>
+//     <button>add to cart</button>
+//   </div>
+// );
+
+// ReactDOM.render(
+//   <Product
+//     price={11.99}
+//     name="Aerosol"
+//     imgUrl="https://off.com.ph/-/media/images/off/ph/products-en/products-landing/landing/off_kids_product_collections_large_2x.jpg?la=en-ph"
+//   />,
+//   document.getElementById("root")
+// );
+
+// Домашнє завдання!!!!!!!!!
+//Створити компонент меню з елементами та передавати в середину всі пункти меню через props
